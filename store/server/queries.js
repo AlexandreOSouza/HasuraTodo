3d5b02c897427cb7ef56184e8b38c9545665b44b
Alexandre <AlexandreOSouza>
2022-07-06 17:48:53 -0300

    chore: add order by to getTodos

--- store/server/queries.js
+++ store/server/queries.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
  export const GET_INCOMPLETE_TODOS = gql`
     query GetTodos($user: String) {
-        todos(where: {todo_user: {_eq: $user}}) {
+        todos(order_by: {todo_id: asc}, where: {todo_user: {_eq: $user}}) {
             todo_id
             todo_mark
             todo_text
